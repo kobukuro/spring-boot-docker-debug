4c4a21f8695e554bbb05471c297c3a7598e333be
PeterChen <sanpeter992000@gmail.com>
2023-08-17 19:41:19 +0800

    Project initialization

---
+++ src/main/java/com/example/sprintbootdockerdebug/SprintBootDockerDebugApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootdockerdebug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootDockerDebugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootDockerDebugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sprintbootdockerdebug/SprintBootDockerDebugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintbootdockerdebug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootDockerDebugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
